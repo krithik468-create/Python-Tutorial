8acddac179266b44b0b72984f62ad15244e2ddcb
unknown <krithikroshan113@gmail.com>
2026-02-01 21:12:52 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,40 @@
+"""
+name1 = "krithik"
+name2 = "roshan"
+name3 = "ansab"
+
+num = int(input("Enter your num:"))
+
+print(num)
+
+if num == 1 :
+    print("my name is",name1)
+elif num == 2 :
+    print("my name is",name2)
+elif num == 3 :
+    print("my name is",name3)
+else:
+    print("no name is found :)")
+    
+"""    
+"""
+name = input("Enter your name:")
+print(f"Hello {name}")
+fav1 = input("What is your favorite animal:")
+fav2 = input("What is your favorite color:")
+fav3 = input("What is your favorite number:")
+print(f"Do you want a {fav2} {fav1} with {fav3} legs?")
+"""
+
+"""x = 3.9
+print(int(x))"""
+
+"""import random
+print(random.randrange(1,10))"""
+
+x = 54
+print(x)
+
+
+
+
---
+++ practice.py
@@ -0,0 +1,93 @@
+"""name = "krithik roshan"
+full_name = f"My name is {name}!"
+
+print(full_name)"""
+
+# mylist = ["apple", "banana", "cherry","cherry","cherry","apple"]
+# ano_list = ["BMW","Benz","hyndai",'ford']
+# mylist.extend(ano_list) #mutable
+# print(ano_list)
+# print(mylist)
+# copy_list = mylist.copy()
+# print(mylist)
+# print("copy_list",copy_list)
+# copy_list.append("ansab")
+# print("copy_list after append",copy_list)
+# print(mylist)
+# emt_value = ""
+# print(mylist.count("cherry"))  #slice method
+# print(len(mylist))
+# mylist.append("krithik")
+# print(mylist.pop(2))
+# mylist.reverse()
+# print(mylist)
+# name = "ansab"
+# mylist.sort()
+# print(mylist.count("apple"))
+# mylist.insert(20,"graps")
+# print(mylist)
+# mylist.clear()
+# print(mylist)
+
+
+# Dict
+# thisdict = {
+#   "brand": "Ford",
+#   "model": "Mustang",
+#   "year":"",
+#   "year": 1964,
+  
+#   "date":122
+# }
+# print(thisdict)
+
+# print("Enter 1 to move next, enter 2 to move back")
+# num = int(input("Next 1 or Back 2 : "))
+# if num > 2 : 
+#   print("Invalid input")
+# else:
+#   match num:
+#     case 1:
+#       print("Move forward ->")
+#     case 2:
+#       print("Move backward <-")
+    
+# adj = ["red", "big", "tasty"]
+# fruits = ["apple", "banana", "cherry"]
+
+# for x in adj:
+#   for y in fruits:
+#     print(x, y)
+
+
+#-------------------------Functions-------------------------------------------------
+
+
+num1 = int(input("Enter 1st number : "))
+num2 = int(input("Enter 2nd number : "))
+
+def add(a, b) :
+ return a + b
+
+def sub(a,b):
+  return a - b
+
+def mul(a, b):
+  return a * b
+
+def rem(a, b):
+  return a % b
+
+def quo(a, b):
+  return a / b
+
+print("Addition :",add(num1,num2))
+print("Subraction :",sub(num1,num2))
+print("Multiplication :",mul(num1,num2))
+print("Reminder :",rem(num1,num2))
+print("Quoitent :",add(num1,num2))
+
+print(f"Entered two numbers are {num1} & {num2}")
+
+
+
